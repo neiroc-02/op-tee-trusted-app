5622298f054095086a9691a5fb5db2c3fe147447
neiroc-02 <neiro.cabrera@yahoo.com>
2025-02-11 16:35:03 -0800

    Hello World sample works with the UUID changing

--- optee_logger/ta/Android.mk
+++ optee_logger/ta/Android.mk
@@ -1,4 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-local_module := 8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta
+#local_module := 8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta
+local_module := 3d969d36-f8fb-4e45-afe4-debefd16ba0e.ta 
+
 include $(BUILD_OPTEE_MK)
